c1d7a7df3ce1b590eae3ef04683e2c95278fe33d
Evgenia-bit <63193897+Evgenia-bit@users.noreply.github.com>
2021-10-21 19:52:30 +0300

    Add files via upload

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,18 @@
 const ApiError = require('../error/ApiError')
 const bcrypt = require('bcrypt')
-const jwt = require('jwt')
+const jwt = require('jsonwebtoken')
 const { User, Basket } = require('../models/models')
 
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        { id, email, role },
+        process.env.SECRET_KEY,
+        { expiresIn: '24h' }
+    )
+}
+
 class UserController {
-    async registration(req, res) {
+    async registration(req, res, next) {
         const { email, password, role } = req.body
         if (!email || !password) {
             return next(ApiError.badRequest('Некорректные данные'))
@@ -15,23 +23,26 @@ class UserController {
         }
         const hashPassword = await bcrypt.hash(password, 5)
         const user = await User.create({ email, role, password: hashPassword })
-        const Basket = await Basket.create({ userId: user.id })
-        const token = jwt.sign(
-            { id: user.id, email, role },
-            process.env.SECRET_KEY,
-            { expiresIn: '24h' }
-        )
+        const basket = await Basket.create({ userId: user.id })
+        const token = generateJwt(user.id, user.email, user.role)
         return res.json(token)
     }
-    async login(req, res) {
-
+    async login(req, res, next) {
+        const {email, password} = req.body
+        const user = await User.findOne({where: {email}})
+        if(!user) {
+            return next(ApiError.internal('Пользователь не найден'))
+        }
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if(!comparePassword) {
+            return next(ApiError.internal("Пароли не совпадают"))
+        }
+        const token =  generateJwt(user.id, user.email, user.role)
+        return res.json(token)
     }
     async check(req, res, next) {
-        const { id } = req.query
-        if (!id) {
-            return next(ApiError.badRequest('не задан id'))
-        }
-        res.json(id)
+        const token = generateJwt(req.user.id, req.user.email, req.user.role)
+        return res.json({token})
     }
 }
 module.exports = new UserController()
